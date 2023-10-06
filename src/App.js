f02627167933c94f963549388a12406eab70422d
RichardXTrusty <ratrusty92@gmail.com>
2023-10-06 11:59:01 -0400

    Installed required NOM packages for React Routing
    -Set up basic routes (Warehouse & Inventory)
    - Added dummy data for visualization

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
-import "./App.scss";
+import React from "react";
 
-function App() {
-  return;
-}
+import { BrowserRouter, Routes, Route, Link, NavLink } from "react-router-dom";
+
+// pages component
+import Warehouse from "./components /Warehouse";
+import Inventory from "./components /Inventory";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <header>
+        <nav>
+          <h1>INSTOCK</h1>
+          <NavLink to="/">Warehouse</NavLink>
+          <NavLink to="Inventory">Inventory</NavLink>
+        </nav>
+      </header>
+      <main>
+        <Routes>
+          <Route index element={<Warehouse />} />
+          <Route path="Inventory" element={<Inventory />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
+  );
+};
 
 export default App;
