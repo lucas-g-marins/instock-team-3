fde0cfa2140b4de35682a7b3ec5d395ec6b56ddf
RichardXTrusty <ratrusty92@gmail.com>
2023-10-06 19:57:40 -0400

    Added dummy code for visualization

--- src/components /Warehouses/Warehouses.js
+++ src/components /Warehouses/Warehouses.js
@@ -27,6 +27,9 @@ const Warehouse = () => {
           </NavLink>
         </div>
       </div>
+
+{/* Created the following table for visualization of code */}
+
       <table>
         <thead>
           <tr>
@@ -37,7 +40,77 @@ const Warehouse = () => {
             <th>ACTIONS</th>
           </tr>
         </thead>
-        <tbody>{/* Add table rows and data here */}</tbody>
+        <tbody>
+          <tr>
+            <td>Manhattan</td>
+            <td>503 Broadway, New York, USA</td>
+            <td>Parmin Aujla</td>
+            <td>
+              +1(629) 555-0129
+              <br />
+              paujla@instock.com
+            </td>
+          </tr>
+          <tr>
+            <td>Washington</td>
+            <td>33 Pearl Street SW, Washington, USA</td>
+            <td>Graeme Lyon</td>
+            <td>
+              +1(647) 504-0911 <br />
+              glyon@instock.com
+            </td>
+          </tr>
+          <tr>
+            <td>New Jersey</td>
+            <td>300 Main Street, New Jersey, USA</td>
+            <td>Brad MacDonald</td>
+            <td>
+              +1(401) 377-2337
+              <br />
+              bmcdonald@instock.com
+            </td>
+          </tr>
+          <tr>
+            <td>San Francisco</td>
+            <td>890 Brannan Street, San Francisco,USA</td>
+            <td>Gary Wong</td>
+            <td>
+              +1(239) 555-0108
+              <br />
+              gwong@instock.com
+            </td>
+          </tr>
+          <tr>
+            <td>Santa Monica</td>
+            <td>520 Broadway, Santa Monica, USA</td>
+            <td>Sharon Ng</td>
+            <td>
+              +1(270) 555-0117
+              <br />
+              sng@instock.com
+            </td>
+          </tr>
+          <tr>
+            <td>Seattle</td>
+            <td>1201 Third Avenue, Seattle, USA</td>
+            <td>Daniel Bachu</td>
+            <td>
+              +1(480)555-0103
+              <br />
+              dbachu@instock.com
+            </td>
+          </tr>
+          <tr>
+            <td>Miami</td>
+            <td>2650 NW 5th Avenue,Miami,USA</td>
+            <td>Alana Thomas</td>
+            <td>
+              +1(647)832-2065
+              <br />
+              athomas@instock.com
+            </td>
+          </tr>
+        </tbody>
       </table>
     </section>
   );
