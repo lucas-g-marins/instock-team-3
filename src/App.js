cb9f19c179b7b3484d4524ce032d2ef3a5a8753c
RichardXTrusty <ratrusty92@gmail.com>
2023-10-10 19:32:19 -0400

    Corrected merge conflicts

--- src/App.js
+++ src/App.js
@@ -1,36 +1,33 @@
 import React from "react";
 
 import "./App.scss";
-import Header from "./components/Header";
-import Inventory from "./pages/Inventory";
-import Warehouses from "./pages/Warehouses";
 import {
   createBrowserRouter,
   Route,
   createRoutesFromElements,
   RouterProvider,
-  Routes
+  Routes,
 } from "react-router-dom";
 // pages component(s) import
-import Warehouse from "./components /Warehouses/Warehouses";
+import Header from "./components /Header";
+import Warehouses from "./components /Warehouses/Warehouses";
 import WarehouseDetails from "./components /WarehouseDetails/WarehouseDetails";
 import Inventory from "./components /Inventory/Inventory";
 // root layout import
 import RootLayout from "./layout/RootLayout";
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<RootLayout />}>
-      <Route index element={<Warehouse />} />
-      <Route path="Inventory" element={<Inventory />} />
-    </Route>
-  )
-);
+// const router = createBrowserRouter(
+//   createRoutesFromElements(
+//     <Route path="/" element={<RootLayout />}>
+//       <Route index element={<Warehouse />} />
+//       <Route path="Inventory" element={<Inventory />} />
+//     </Route>
+//   )
+// );
 
-const App = () => {
-  return <RouterProvider router={router} />;
-};
-
+// const App = () => {
+//   return <RouterProvider router={router} />;
+// };
 
 function App() {
   return (
