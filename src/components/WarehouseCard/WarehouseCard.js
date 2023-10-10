10ece598497302831b20edb95ebc35993970ea78
lucas-g-marins <lhintzmarins@gmail.com>
2023-10-09 18:20:10 -0700

    add styling for search and button

---
+++ src/components/WarehouseCard/WarehouseCard.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./WarehouseCard.scss";
+
+function WarehouseCard({}) {
+  return (
+    <div className="warehousecard">
+      <div className="warehousecard__location">
+        <h4>WAREHOUSE</h4>
+      </div>
+      <div className="warehousecard__contact"></div>
+    </div>
+  );
+}
+
+export default WarehouseCard;
--- src/components/WarehouseList/WarehouseList.js
+++ src/components/WarehouseList/WarehouseList.js
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./WarehouseList.scss";
+import WarehouseCard from "../WarehouseCard/WarehouseCard";
+import axios from "axios";
 
 function WarehouseList({}) {
+  const [warehouseData, setWarehouseData] = useState([]);
+
+  const apiURL = process.env.REACT_APP_DATA;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data } = await axios.get(`${apiURL}/warehouses`);
+      setWarehouseData(data);
+    };
+    fetchData();
+  }, []);
+
   return (
-    <div>
-      <h1>Warehouses</h1>
-      <form>
+    <div className="warehouses">
+      <h1 className="warehouses__title">Warehouses</h1>
+      <form className="warehouses__form">
         <input type="text" name="search" placeholder="Search..."></input>
+        <button className="warehouses__button">
+          <h3>+ Add New Warehouse</h3>
+        </button>
       </form>
+      <WarehouseCard />
     </div>
   );
 }
