b1b82ab2df58490ff8ceb0f0c7ff696a4d9994a5
Sarbjot Marwaha <sarbjoto1@outlook.com>
2023-10-10 19:41:34 -0400

    add-footer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-import "./App.scss";
+import React from 'react';
+import EditWarehouse from './components/EditWarehouse/EditWarehouse'; // Import the EditWarehouse component
+import Footer from './components/Footer/Footer'; // Import the footer component
 
-function App() {
-  return;
-}
+const App = () => {
+  return (
+    <div className="app">
+      <EditWarehouse />
+      <Footer />
+    </div>
+  );
+};
 
 export default App;
