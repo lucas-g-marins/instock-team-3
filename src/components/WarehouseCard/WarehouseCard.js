220c82a0bbc1a6a8621b4ac429a67b7b658990d6
lucas-g-marins <lhintzmarins@gmail.com>
2023-10-10 22:08:12 -0700

    edit styling for tablet

--- src/components/WarehouseCard/WarehouseCard.js
+++ src/components/WarehouseCard/WarehouseCard.js
@@ -1,16 +1,42 @@
 import React from "react";
 import "./WarehouseCard.scss";
+import DeleteIcon from "../../assets/icons/delete_outline-24px.svg";
+import EditIcon from "../../assets/icons/edit-24px.svg";
+import ChevronIcon from "../../assets/icons/chevron_right-24px.svg";
 
-function WarehouseCard({ city }) {
+function WarehouseCard({ city, address, name, phone, email }) {
   return (
     <div className="warehousecard">
-      <div className="warehousecard__location">
-        <h4>WAREHOUSE</h4>
-        <h4 className="warehousecard__city">{city}</h4>
-        <h4>ADDRESS</h4>
-        <h4></h4>
+      <div className="warehousecard__info">
+        <div className="warehousecard__location">
+          <h4 className="warehousecard__copy warehousecard__title">
+            WAREHOUSE
+          </h4>
+          <div className="warehousecard__name-container">
+            <h3 className="warehousecard__city">{city}</h3>
+            <img src={ChevronIcon}></img>
+          </div>
+          <h4 className="warehousecard__copy warehousecard__title">ADDRESS</h4>
+          <h4 className="warehousecard__copy">{address}</h4>
+        </div>
+        <div className="warehousecard__contact">
+          <h4 className="warehousecard__copy warehousecard__title">
+            CONTACT NAME
+          </h4>
+          <h4 className="warehousecard__copy">{name}</h4>
+          <h4 className="warehousecard__copy warehousecard__title">
+            CONTACT INFORMATION
+          </h4>
+          <div className="warehousecard__contact-info">
+            <h4 className="warehousecard__copy">{phone}</h4>
+            <h4 className="warehousecard__copy">{email}</h4>
+          </div>
+        </div>
       </div>
-      <div className="warehousecard__contact"></div>
+      <div className="warehousecard__icons">
+        <img src={DeleteIcon}></img>
+        <img src={EditIcon}></img>
+      </div>
     </div>
   );
 }
--- src/components/WarehouseList/WarehouseList.js
+++ src/components/WarehouseList/WarehouseList.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import "./WarehouseList.scss";
 import WarehouseCard from "../WarehouseCard/WarehouseCard";
 import axios from "axios";
+import SearchIcon from "../../assets/icons/search-24px.svg";
 
 function WarehouseList({}) {
   const [warehouseData, setWarehouseData] = useState([]);
@@ -18,15 +19,34 @@ function WarehouseList({}) {
 
   return (
     <div className="warehouses">
-      <h1 className="warehouses__title">Warehouses</h1>
-      <form className="warehouses__form">
-        <input type="text" name="search" placeholder="Search..."></input>
-        <button className="warehouses__button">
-          <h3>+ Add New Warehouse</h3>
-        </button>
-      </form>
+      <div className="warehouses__header">
+        <h1 className="warehouses__title">Warehouses</h1>
+        <form className="warehouses__form">
+          <div className="warehouses__input-container">
+            <input type="text" name="search" placeholder="Search..."></input>
+            <img className="warehouses__search-icon" src={SearchIcon}></img>
+          </div>
+          <button className="warehouses__button">
+            <h3>+ Add New Warehouse</h3>
+          </button>
+        </form>
+      </div>
+      <div className="warehouses__sections">
+        <h4>WAREHOUSE</h4>
+        <h4>ADDRESS</h4>
+        <h4>CONTACT NAME</h4>
+        <h4>CONTACT INFORMATION</h4>
+        <h4>ACTIONS</h4>
+      </div>
       {warehouseData.map((warehouse) => (
-        <WarehouseCard key={warehouse.id} city={warehouse.city} />
+        <WarehouseCard
+          key={warehouse.id}
+          city={warehouse.city}
+          address={warehouse.address}
+          name={warehouse.contact_name}
+          phone={warehouse.contact_phone}
+          email={warehouse.contact_email}
+        />
       ))}
     </div>
   );
