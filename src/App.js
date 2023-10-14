90a869a8f5bce3a59aaf822a94f06d914db9dedf
Thais De Macedo <thademacedo@gmail.com>
2023-10-14 08:50:49 -0700

    - Created InventoryList and InventoryCard

--- src/App.js
+++ src/App.js
@@ -13,9 +13,11 @@ import {
 import Header from "./components/Header/Header";
 import WarehouseList from "./components/WarehouseList/WarehouseList";
 import InventoryList from "./components/InventoryList/InventoryList";
-import WarehouseCard from "./components/WarehouseCard/WarehouseCard";
+// import WarehouseCard from "./components/WarehouseCard/WarehouseCard";
+// import InventoryCard from "./components/InventoryCard/InventoryCard";
 import EditWarehouse from "./components/EditWarehouse/EditWarehouse"; // Import the EditWarehouse component
 import Footer from "./components/Footer/Footer"; // Import the footer component
+import InventoryCard from "./components/InventoryCard/InventoryCard";
 
 function App() {
   return (
@@ -24,7 +26,7 @@ function App() {
       {/* <WarehouseList /> */}
       <Routes>
         <Route path={"/WarehouseList"} element={<WarehouseList />} />
-        <Route path={"/WarehouseCard"} element={<WarehouseCard />} />
+        {/* <Route path={"/WarehouseCard"} element={<WarehouseCard />} /> */}
         <Route path={"/EditWarehouse"} element={<EditWarehouse />} />
         <Route path={"/InventoryList"} element={<InventoryList />} />
       </Routes>
