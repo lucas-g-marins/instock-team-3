ff3cb72d65898965cf9dac5e5e81710b6f7577cc
RichardXTrusty <ratrusty92@gmail.com>
2023-10-14 20:09:44 -0400

    AP3-13 Front-End: Delete Warehouse Modal/Component (Responsive)

--- src/components/DeleteWarehouse/DeleteWarehouse.js
+++ src/components/DeleteWarehouse/DeleteWarehouse.js
@@ -2,10 +2,11 @@ import * as React from "react";
 import axios from "axios";
 import { Unstable_Popup as Popup } from "@mui/base/Unstable_Popup";
 import { styled } from "@mui/system";
+import "./DeleteWarehouse.scss";
 
 export default function DeleteWarehouse({ warehouseid, warehousename, icon }) {
   const apiURL = process.env.REACT_APP_DATA;
-  //   const [anchor, setAnchor] = (React.useState < null) | (HTMLElement > null);
+    // const [anchor, setAnchor] = (React.useState < null) | (HTMLElement > null);
   const [open, setOpen] = React.useState(false);
 
   const deleteWarehouse = async () => {
@@ -14,7 +15,7 @@ export default function DeleteWarehouse({ warehouseid, warehousename, icon }) {
         `${apiURL}/warehouses/${warehouseid}`
       );
       console.log(data);
-      //   setWarehouseData(...data);
+        // setWarehouseData(...data);
     } catch (error) {
       console.log("Error:", error);
     }
@@ -24,25 +25,29 @@ export default function DeleteWarehouse({ warehouseid, warehousename, icon }) {
     <div>
       <div onClick={() => setOpen(true)}>{icon}</div>
 
-      <Popup open={open}>
-        <PopupBody>
+      <Popup className="modalcontainer" open={open}>
+        <PopupBody className="modal">
           <h1>Delete {warehousename} warehouse?</h1>
-          <p>
+          <p className="modalText">
             Please confirm that you'd like to delete the {warehousename} from
             the list of warehouses. You won't be able to undo the section.
           </p>
-          <button
-            onClick={() => {
-              setOpen(false);
-            }}>
-            Cancel
-          </button>
-          <button
-            onClick={() => {
-              deleteWarehouse();
-            }}>
-            Detele
-          </button>
+          <div className="buttonContainer">
+            <button
+              className="cancelButton"
+              onClick={() => {
+                setOpen(false);
+              }}>
+              Cancel
+            </button>
+            <button
+              className="deleteButton"
+              onClick={() => {
+                deleteWarehouse();
+              }}>
+              Detele
+            </button>
+          </div>
         </PopupBody>
       </Popup>
     </div>
@@ -63,48 +68,5 @@ const blue = {
   700: "#0059B2",
 };
 
-const PopupBody = styled("div")(
-  ({ theme }) => `
-  width: max-content;
-  padding: 12px 16px;
-  margin: 8px;
-  border-radius: 8px;
-  border: 1px solid ${theme.palette.mode === "dark" ? grey[700] : grey[200]};
-  background-color: ${theme.palette.mode === "dark" ? grey[900] : grey[50]};
-  box-shadow: ${
-    theme.palette.mode === "dark"
-      ? `0px 4px 8px rgb(0 0 0 / 0.7)`
-      : `0px 4px 8px rgb(0 0 0 / 0.1)`
-  };
-  font-family: 'IBM Plex Sans', sans-serif;
-  font-size: 0.875rem;
-  z-index: 1;
-`
-);
+const PopupBody = styled("div")();
 
-// const Button = styled("button")`
-//   font-family: "IBM Plex Sans", sans-serif;
-//   font-size: 0.875rem;
-//   line-height: 1.5;
-//   background-color: ${blue[500]};
-//   color: white;
-//   border-radius: 8px;
-//   font-weight: 600;
-//   padding: 8px 16px;
-//   cursor: pointer;
-//   transition: all 150ms ease;
-//   border: none;
-
-//   &:hover {
-//     background-color: ${blue[600]};
-//   }
-
-//   &:active {
-//     background-color: ${blue[700]};
-//   }
-
-//   &:focus-visible {
-//     box-shadow: 0 4px 20px 0 rgb(61 71 82 / 0.1), 0 0 0 5px rgb(0 127 255 / 0.5);
-//     outline: none;
-//   }
-// `;
